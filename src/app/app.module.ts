d9c4abffead3d0c6f310f74caea0f6cbc2fcc11b
Leonardo Pereira Rodrigues <brutaleo@hotmail.com>
2021-12-15 15:53:36 -0300

    Montagem da estrutura inicial de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OrdemDeServicoComponent } from './ordem-de-servico/ordem-de-servico.component';
+import { HeaderComponent } from './header/header.component';
+import {FormsModule} from "@angular/forms";
+import {MaterialModule} from "./material.module";
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrdemDeServicoComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
+    MaterialModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ordem-de-servico/ordem-de-servico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ordem-de-servico',
+  templateUrl: './ordem-de-servico.component.html',
+  styleUrls: ['./ordem-de-servico.component.css']
+})
+export class OrdemDeServicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
