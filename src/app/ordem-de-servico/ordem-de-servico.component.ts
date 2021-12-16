4bfce3928563f7fe7f83d7317cde085570844761
Leonardo Pereira Rodrigues <brutaleo@hotmail.com>
2021-12-16 11:06:24 -0300

    Adição de botão EXAMES no componente Ordem de Serviço

--- src/app/ordem-de-servico/ordem-de-servico.component.ts
+++ src/app/ordem-de-servico/ordem-de-servico.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {OrdemDeServico} from "../modelo/OrdemDeServico";
 import {MatDialog} from "@angular/material/dialog";
 import {OrdemDeServicoEdicaoComponent} from "../ordem-de-servico-edicao/ordem-de-servico-edicao.component";
+import {OrdemDeServicoExame} from "../modelo/OrdemDeServicoExame";
 
 export const ORDENS_DATA = [
   {id: '1', convenio: 'XPTO', medico: 'MD. House', paciente: 'Leonardo', postoColeta: 'Posto1', dataDeCadastro: '14/12/2021', dataDeAtualizacao: '14/12/2021',  protocolo: '123456789'},
@@ -15,7 +16,7 @@ export const ORDENS_DATA = [
 })
 export class OrdemDeServicoComponent implements OnInit {
   //public dataSource: OrdemDeServico[] = [];
-  public dataSource: ({ protocolo: string; postoColeta: string; paciente: string; medico: string; convenio: string; dataDeCadastro: string; id: string; dataDeAtualizacao: string } | { protocolo: string; postoColeta: string; paciente: string; medico: string; convenio: string; dataDeCadastro: string; id: string; dataDeAtualizacao: string })[] = ORDENS_DATA;
+  public dataSource: ({ protocolo: string; postoColeta: string; paciente: string; medico: string; convenio: string; dataDeCadastro: string; id: string; dataDeAtualizacao: string })[] = ORDENS_DATA;
   public displayedColumns: string[] = ['id', 'convenio', 'medico', 'paciente', 'postoColeta', 'dataDeCadastro', 'protocolo', 'acoes'];
   constructor(private dialog: MatDialog) { }
 
@@ -53,6 +54,10 @@ export class OrdemDeServicoComponent implements OnInit {
   }
 
   public gerarProtocolo(ordemDeServico: OrdemDeServico) {
+
+  }
+
+  public adicionarExames(ordemDeServicoExame: OrdemDeServicoExame) {
 
   }
 }
